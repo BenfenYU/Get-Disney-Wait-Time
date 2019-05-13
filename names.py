384767e85d8d4a3abd5895202c661086a4a9ff18
benfenyu <yujingbo_bf@163.com>
2019-05-13 18:40:12 +0800

    init

---
+++ names.py
@@ -0,0 +1,57 @@
+# -*-coding:utf8-*-
+"""
+This file contains all name mapping from English to Chinese.
+"""
+
+NAMES = {
+    'MeetDroidFriends': u'？？？',
+    'TronLightcyclePowerRun': u'创极速光轮',
+	'RoaringRapids': u'雷鸣山漂流',
+	'ChallengeTrails': u'古迹探索营的绳索挑战道',
+	'VistaTrail': u'古迹探索营的探索步行道',
+	'1879': u'？？？',
+	'OnceUponTimeAdventure': u'漫游童话时光',
+	'AliceWonderlandMaze': u'爱丽丝梦游仙境迷宫',
+	'1882': u'？？？',
+	'HunnyPotSpin': u'旋转疯蜜罐',
+	'DisneyPrincessesStorybookCourt': u'？？？',
+	'PeterPansFlight': u'小飞侠天空奇遇',
+	'SevenDwarfsMineTrain': u'七个小矮人矿山车',
+	'AdventuresWinniePooh': u'小熊维尼历险记',
+	'VoyageToCrystalGrotto': u'晶彩奇航',
+	'DumboFlyingElephant': u'小飞象',
+	'FantasiaCarousel': u'旋转木马',
+	'1900': u'漫威英雄总部',
+	'1913': u'？？？',
+	'ExplorerCanoes': u'探险家独木舟',
+	'PiratesOfCaribbean': u'加勒比海盗-沉落宝藏之战',
+	'ShipwreckShore': u'船奇戏水滩',
+	'SirensRevenge': u'探秘海妖复仇号',
+	'BuzzLightyearPlanetRescue': u'巴斯光年星际营救',
+	'JetPacks': u'喷气背包飞行器',
+	'StitchEncounter': u'太空幸会史迪奇',
+	'1905': u'？？？',
+	'SoaringOverHorizon': u'翱翔.飞跃地平线',
+	'MeetMickeyGardensImagination': u'米奇俱乐部',
+	'CaptainAmerica': u'漫威英雄总部-美国队长',
+	'JungleFriendsHappyCircle': u'欢笑聚友会',
+	'CampDiscovery': u'？？？',
+	'MickeysFilmFestival': u'米奇电影节',
+	'TronRealm': u'？？？',
+	'StarWarsLaunchBay': u'',
+	'MarvelUniverse': u'十二朋友园',
+	'2596': u'？？？',
+	'SpiderMan': u'漫威英雄总部-蜘蛛侠',
+	'BecomeIronMan': u'漫威英雄总部-钢铁侠',
+	'ScreeningRoom': u'电影放映室',
+	'EncounterKyloRen': u'？？？',
+	'MeetDarthVader': u'？？？',
+	'MillenniumFalcon': u'？？？',
+    'RexsRCRacer': u'抱抱龙冲天赛车',
+    'SlinkyDogSpin': u'弹簧狗团团转',
+    'WoodysRoundUp': u'胡迪牛仔嘉年华',
+}
+
+
+def translate(en):
+    return NAMES.get(en)
